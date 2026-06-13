6dc706d2e227c57c3080dd4573c2ec7505580030
Shaikh Aliya <aliyashk71@gmail.com>
2026-06-14 00:17:31 +0530

    Exp-2.py
    Python Code for calculation of area of rectangle

--- Exp-2.py
+++ Exp-2.py
@@ -6,3 +6,10 @@ length = float(input("Enter the length of the rectangle: "))
 width = float(input("Enter the width of the rectangle: "))
 area = length * width
 print("The area of the rectangle is:", area)
+
+'''
+area of rectangle
+Enter the length of the rectangle: 10
+Enter the width of the rectangle: 8
+The area of the rectangle is: 80.0
+'''
