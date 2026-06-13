9ff49b4567d811f1116de9d0ef431b98eb6be52f
Shaikh Aliya <aliyashk71@gmail.com>
2026-06-14 00:14:11 +0530

    Update and rename README.md to Exp-2.py
    Python Code for calculation of area of rectangle.

--- Exp-2.py
+++ Exp-2.py
@@ -1,6 +1,6 @@
-#Coder:Aliya Shaikh
-#Code for finding area of rectangle
+# College Python Experiments
 
+## Experiment 2: Area of Rectangle
 print("area of rectangle")
 length = float(input("Enter the length of the rectangle: "))
 width = float(input("Enter the width of the rectangle: "))
